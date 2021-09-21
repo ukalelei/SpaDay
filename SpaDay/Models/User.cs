295a789787f1a4a025bd57c8ca71e59d353809af
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-09-20 23:45:39 -0500

    create new properties with constructors

---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public DateTime DateJoined { get; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+            DateJoined = DateTime.Now;
+        }
+
+        public User(string userName, string email, string password): this ()
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
