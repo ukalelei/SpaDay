6e2d5e5fbcbd3e248baa246176bdb41f0d858da5
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-09-20 23:48:54 -0500

    create action method Add() to render form. Create action method SubmitAddUserForm() to process form submission

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController: Controller
+    {
+      
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.userName = newUser.UserName;
+            ViewBag.email = newUser.Email;
+            ViewBag.password = newUser.Password;
+            ViewBag.verify = verify;
+            ViewBag.dateJoined = newUser.DateJoined;
+
+            if (newUser.Password == verify)
+            {
+                ViewBag.isError = false;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.isError = true;
+                ViewBag.ErrorMessage = "Your password do not match. Try again";
+                return View("Add");
+            }
+        }
+    }
+}
