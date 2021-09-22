2fc0fc3b79365456de1faca5d64487c06cb539dc
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-09-22 01:26:43 -0500

    change wording of error message

--- SpaDay/ViewModels/AddUserViewModel.cs
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -6,11 +6,11 @@ namespace SpaDay.ViewModels
     public class AddUserViewModel
     {
         [Required(ErrorMessage = "Username required")]
-        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username can only be 5 to 15 characters long")] //Username is between 5 and 15 characters.
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username must contain 5 to 15 characters")] //Username is between 5 and 15 characters.
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password required")]
-        [StringLength(20, MinimumLength = 6, ErrorMessage = "Passowrd can only be 6 to 20 characters long")] //password is between 6 and 20 characters
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Passowrd must contain 6 to 20 characters")] //password is between 6 and 20 characters
         public string Password { get; set; }
         
         [EmailAddress(ErrorMessage = "Not a valid email address")]
